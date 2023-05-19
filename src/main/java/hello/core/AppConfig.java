ad937ed0dfc08908cf3b40565e90a2f742b8983e
inkwon <dlsrnjs125@naver.com>
2023-05-19 16:02:39 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,18 +16,18 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     // commend + option + m -> 리팩터링
     @Bean
     public MemoryMemberRepository memberRepository() {
-
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
-
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
