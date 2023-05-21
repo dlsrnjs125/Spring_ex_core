b9055f2b307d45116dfd1209b1f80569ea94ec5b
inkwon <dlsrnjs125@naver.com>
2023-05-21 21:40:29 +0900

    @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -34,4 +34,14 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDepp() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+        // 출력 : bean.getClass() = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$fddc83ae
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
 }
